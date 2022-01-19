e8b8fbf61a8eca4a97adb50dfa7ad2a29fe14411
josephhood <ec2-user@ip-172-31-49-190.ec2.internal>
2022-01-19 04:02:51 +0000

    updated build process and file structure

---
+++ hello/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << endl << "HELLO CLOUD9!" << endl;
+    return 0;
+}
--- test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello Cloud9!";
-    return 0;
-}
